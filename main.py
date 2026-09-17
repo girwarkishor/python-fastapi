9a8250eea86d5b91f9ecfe71cde515298c04e9a2
Girwar Kishor Chandravanshi <girwar.kishor@gmail.com>
2026-09-17 10:10:03 +0000

    wikipedia logic added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from python-fastapi!")
+from mylib.logics import get_wikipedia_summary
 
+def main():
+    summary = get_wikipedia_summary()
+    print(summary)
 
 if __name__ == "__main__":
     main()
---
+++ mylib/logics.py
@@ -0,0 +1,12 @@
+import wikipedia
+
+# ApplicationName/Version (ProjectURL or ContactInformation)
+wikipedia.set_user_agent(
+    "python-fastapi/0.1 (https://github.com/girwarkishor/python-fastapi)"
+)
+
+
+def get_wikipedia_summary(name: str = "War Goddess", length: int = 1) -> str:
+    """Get the summary of a Wikipedia page for a given query."""
+    my_wiki = wikipedia.summary(name, length)
+    return my_wiki
