8b482e9c71d719323dda0c21c53df804b0a12721
Girwar Kishor Chandravanshi <girwar.kishor@gmail.com>
2026-09-25 11:12:59 +0000

    test cases fixed

--- test/mylib/test_logics.py
+++ test/mylib/test_logics.py
@@ -1,5 +1,8 @@
+import asyncio
+
 from mylib.logics import get_wikipedia_summary
 
+
 def test_get_wikipedia_summary():
-    summary = get_wikipedia_summary()
+    summary = asyncio.run(get_wikipedia_summary())
     assert "god" in summary
